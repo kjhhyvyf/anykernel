25e5671a8ccfe268c196e2c62c7099e217fdcc1d
烤坤 <3332539702@qq.com>
2025-09-16 21:35:21 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,39 +1,3 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
-
-## AnyKernel setup
-# begin properties
-properties() { '
-  do.devicecheck=0
-do.modules=0
-do.systemless=1
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=alioth
-device.name2=aliothin
-device.name3=apollo
-device.name4=apolloin
-device.name5=lmi
-supported.versions=
-supported.patchlevels=
-'; } # end properties
-
-# shell variables
-block=boot;
-is_slot_device=auto;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-no_block_display=1
-
-## Import AnyKernel core functions
-. tools/ak3-core.sh
-
-ui_print ""
-ui_print "  =================================================="
-ui_print "              KaoKun Kernel Installer          "
-ui_print "  =================================================="
-ui_print ""
-ui_print "    Target Device  : Xiaomi Pad 6 (pipa)"
 ui_print "    Chipset        : Snapdragon 870"
 ui_print "    Author         : KaoKun"
 ui_print ""
